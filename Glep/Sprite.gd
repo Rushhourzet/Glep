f531da81f5fc48a6a9256b9db89569b5c41ca81b
Rushhourz <sascha.hampl@outlook.com>
2020-01-23 16:13:04 +0100

    some art lul

--- Glep/Sprite.gd
+++ Glep/Sprite.gd
@@ -1,13 +1,23 @@
 extends Sprite
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (int) var speed = 200
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var velocity = Vector2()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('right'):
+		        velocity.x += 100
+	if Input.is_action_pressed('left'):
+		velocity.x -= 100
+	if Input.is_action_pressed('down'):
+		velocity.y += 100
+	if Input.is_action_pressed('up'):
+		velocity.y -= 100
+		print("up")
+	velocity = velocity * speed
+
+func _physics_process(delta):
+	get_input()
+	$PlayerBody2D.add_central_force(velocity)
+	print(velocity)
