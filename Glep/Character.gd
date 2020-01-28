0845fe02c370de0e00cc43a2b972b0f81969a464
Rushhourz <sascha.hampl@outlook.com>
2020-01-28 22:33:34 +0100

    Controller progress

--- Glep/Character.gd
+++ Glep/Character.gd
@@ -1,18 +1,39 @@
 extends RigidBody
 
-var moveSpeed = 200
-var jumpForce = 400
-var rotationSpeed = 0.4
+var moveSpeed = 20
+var jumpForce = 40
+var rotationSpeed = 0.01
 var velocity = Vector3()
+var dashFactor = 5
+var jump = false
+var doubleJump = true
 
 
 
 
 func _physics_process(delta):
 	apply_impulse(Vector3(), get_input() * delta * moveSpeed)
+	
+	#Grounded or on Wall
+	if contacts_reported > 1:
+		jump = true
+	#jump
+	if Input.is_action_just_pressed("jump") and jump and $jumpCD.is_stopped():
+		apply_impulse(Vector3(), Vector3.UP * jumpForce)
+		$jumpCD.start()
+		jump = false
+	#doublejump
+	if Input.is_action_just_pressed("jump") and contacts_reported == 0 and doubleJump:
+		apply_impulse(Vector3(), Vector3.UP * jumpForce)
+		doubleJump = false
+	if Input.is_action_just_pressed("dash") and $dashCD.is_stopped():
+		apply_impulse(Vector3(), get_input() * delta * moveSpeed * dashFactor)
+		
 
 func _unhandled_input(event):
-	pass
+	if event is InputEventMouseMotion:
+		$Camera.rotate_x(-event.relative.y * rotationSpeed)
+		rotate_y(-event.relative.x * rotationSpeed)
 	
 func get_input():
 	velocity = Vector3()
--- Glep/PlayerBody2D.gd
+++ Glep/PlayerBody2D.gd
@@ -1,7 +1,6 @@
 extends RigidBody2D
 
-export (int) var speed = 200
-export (float) var _friction = 5
+export (int) var speed = 500
 
 var velocity = Vector2()
 
@@ -9,48 +8,16 @@ func get_input():
     velocity = Vector2()
     if Input.is_action_pressed('right'):
         velocity.x += 1
-        return true
     if Input.is_action_pressed('left'):
         velocity.x -= 1
-        return true
     if Input.is_action_pressed('down'):
         velocity.y += 1
-        return true
     if Input.is_action_pressed('up'):
         velocity.y -= 1
-        return true
-    return false
 
 func _physics_process(delta):
 	get_input()
-	velocity = velocity.normalized() * speed
-	add_force(velocity, velocity*delta )
+	velocity = velocity.normalized() * speed * delta
+	apply_impulse(Vector2(), velocity)
 	print (velocity)
 	print (linear_velocity * delta)
-
-
-func applyFriction1():
-	print("a")
-	if linear_velocity.x > 0:
-		velocity.x -= _friction
-	if linear_velocity.x < 0:
-		velocity.x += _friction
-	if linear_velocity.y > 0:
-		velocity.y -= _friction
-	if linear_velocity.y < 0:
-		velocity.y += _friction
-
-func applyFriction2():
-	print("a")
-	if linear_velocity.x > 0:
-		set_axis_velocity(Vector2(cubeRoot(linear_velocity.x), linear_velocity.y))
-	if linear_velocity.x < 0:
-		set_axis_velocity(Vector2(cubeRoot(linear_velocity.x), linear_velocity.y))
-	if linear_velocity.y > 0:
-		set_axis_velocity(Vector2(linear_velocity.x, cubeRoot(linear_velocity.y)))
-	if linear_velocity.y < 0:
-		set_axis_velocity(Vector2(linear_velocity.x, cubeRoot(linear_velocity.y)))
-		
-func cubeRoot(var x):
-	if x>0: return x*(1.0/3.0)
-	return -(-x)* (1.0/3.0)
