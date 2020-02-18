6c40a8ec3b5175e6b60bce2f4e5df6668ced8d0e
Rushhourz <sascha.hampl@outlook.com>
2020-02-18 22:47:37 +0100

    Basic Movement, deleting some assets

--- Glep/Player.cs
+++ Glep/Player.cs
@@ -4,7 +4,7 @@ using System;
 public class Player : RigidBody
 {
 	Camera camera;
-	float cameraSens = 0.01f, movementForce;
+	float cameraSens = 0.001f, movementForce = 1000f;
 
 	public override void _Ready()
 	{
@@ -12,13 +12,46 @@ public class Player : RigidBody
 	}
 
 	public override void _Process(float delta){
-		GD.Print(camera.Rotation.x);
+		// GD.Print(camera.Rotation.x);
 	}
 
-	public override void _PhysicsProcess(float delta){
-		if(Input.IsActionPressed("move_forward")){
-			
+	public override void _PhysicsProcess(float delta)
+	{
+		ApplyCentralImpulse(
+			getMovementForceVector(Transform.basis.z.Normalized(), Transform.basis.x.Normalized(), Transform.basis.y.Normalized()) 
+			* movementForce * delta);
+		if(Input.IsActionPressed("gravity_toggle")){
+			if(GravityScale == 1){
+				GravityScale = 0;
+			}
+			else{
+				GravityScale = 1;
+			}
 		}
+	}
+
+	private static Vector3 getMovementForceVector(Vector3 forward, Vector3 right, Vector3 up){
+		Vector3 moveDirection = new Vector3();
+		if (Input.IsActionPressed("move_forward")){
+			moveDirection += Vector3.Forward;
+		}
+		if (Input.IsActionPressed("move_back")){
+			moveDirection += Vector3.Back;
+		}
+		if (Input.IsActionPressed("move_left")){
+			moveDirection += Vector3.Left;
+		}
+		if (Input.IsActionPressed("move_right")){
+			moveDirection += Vector3.Right;
+		}
+		if (Input.IsActionPressed("move_up")){
+			moveDirection += Vector3.Up;
+		}
+		if (Input.IsActionPressed("move_down")){
+			moveDirection += Vector3.Down;
+		}
+		Vector3 movement = forward * moveDirection.z + right * moveDirection.x + up * moveDirection.y;		
+		return movement;
 	}
 
 	public override void _UnhandledInput(InputEvent @event){
